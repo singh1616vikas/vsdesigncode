d0f29b484934b87980def050fa85ca08d7fd332a
singh1616vikas <126976204+singh1616vikas@users.noreply.github.com>
2024-01-10 23:28:39 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+
+var typed = new Typed(".auto-type", {
+    strings: ["Web Designer", "Web Developer", "Graphics Designer"],
+    typeSpeed: 160,
+    backSpeed: 160,
+    loop: true
+})
+
+    // form validation
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+
+
+// data-filter
